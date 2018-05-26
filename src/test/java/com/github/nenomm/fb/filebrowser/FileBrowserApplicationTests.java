3f5dd809dba48ae1dacb458be3bd9da32736c1f2
nenad <nenad.nenadovic@kapsch.net>
2018-05-26 18:39:52 +0200

    add project related files

---
+++ src/test/java/com/github/nenomm/fb/filebrowser/FileBrowserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.nenomm.fb.filebrowser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileBrowserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
